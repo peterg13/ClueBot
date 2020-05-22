84075cebfbdedb5e168ef6a851f14903a53559b0
Peter Gustafson <petergustafson19@gmail.com>
2020-05-21 20:23:38 -0700

    Update views.py

--- ClueSite/clue_bot/views.py
+++ ClueSite/clue_bot/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from cards.card_handler import loadCards
 
 # Create your views here.
 
 def home(request):
+    suspects, weapons, rooms = loadCards()
+    print(suspects)
     return render(request, 'clue_bot/home.html')
