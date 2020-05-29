d61f39906ad9aabdca79ed272312da583afc27c0
Peter Gustafson <petergustafson19@gmail.com>
2020-05-28 21:30:47 -0700

    getting basics of css together to build site

--- ClueSite/clue_bot/views.py
+++ ClueSite/clue_bot/views.py
@@ -4,6 +4,12 @@ from clue_bot.cards.card_handler import loadCards
 # Create your views here.
 
 def home(request):
+    #suspects - list of class cards that contain the suspects
+    #weapons - list of class cards that contain the suspects
+    #rooms - list of class cards that contain the suspects
     suspects, weapons, rooms = loadCards()
-    print(suspects[0].getName())
+    
+
+
+
     return render(request, 'clue_bot/home.html')
